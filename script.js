689f595102d667ace9a48a1d258ec560b80f3dec
Brenda Rayane <brenda.rayane.0018@gmail.com>
2022-08-19 23:46:27 -0300

    verso da carta adicionado aleatoriamente

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ let quantidadeDeCartas = Number(prompt("Escolha a quantidade de cartas de 4 a 14
   while (quantidadeDeCartas % 2 != 0 || quantidadeDeCartas > 14 || quantidadeDeCartas < 4) {
     quantidadeDeCartas = Number(prompt("Escolha a quantidade de cartas de 4 a 14:")); 
   }
+
+const gifsparrot=[
+    "bobrossparrot.gif", 
+    "explodyparrot.gif", 
+    "fiestaparrot.gif", 
+    "metalparrot.gif", 
+    "revertitparrot.gif", 
+    "tripletsparrot.gif", 
+    "unicornparrot.gif",
+    "bobrossparrot.gif", 
+    "explodyparrot.gif", 
+    "fiestaparrot.gif", 
+    "metalparrot.gif", 
+    "revertitparrot.gif", 
+    "tripletsparrot.gif", 
+    "unicornparrot.gif" ];
+    console.log(gifsparrot);
+
 function addCards(){
     let listaDeCartas=[];
     for(i=0; i<quantidadeDeCartas;i++){
      listaDeCartas.push(
     `<li>
-         <div class="card card${i}" onclick="virarCartas(this)">
-            <div class="face front"><img src="front.png" class="parrot Pfront"></div>
-            <div class="face back"><img src="./fiestaparrot.gif" class="parrot Pback"></div>
-         </div>
+        <div class="card face card${i}">
+            <img src="front.png" class="front parrot">
+            <div class="backInativa"><img src=${gifsparrot[i]}> </div>   
+        </div>
     </li>`)
     }
 
@@ -27,8 +45,6 @@ function addCards(){
    
 }
 
-function virarCartas(cartaEscolhida){
-
-}
+//function virarCartas(cardEscolhido){}
    addCards();
       
